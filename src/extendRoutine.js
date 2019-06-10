3b1c11890e646166ccc3c8fafe6ca17e7cff5a8a
Yury Shapkarin <yury@shapkarin.me>
2019-06-10 16:33:51 +0300

    spread

--- src/extendRoutine.js
+++ src/extendRoutine.js
@@ -1,7 +1,7 @@
 import { createAction } from 'redux-actions';
 
 const createActionCreator = ({ type, typePrefix }) => createAction(`${typePrefix}/${type}`);
-const toCamelCase = string => string.toLowerCase().replace(/[_.\- ]+(\w|$)/g, (_, p1) => p1.toUpperCase())
+const toCamelCase = string => string.toLowerCase().replace(/[_.\- ]+(\w|$)/g, (_, p1) => p1.toUpperCase());
 
 export default function extendRoutine(routine, types = []){
   const typePrefix = routine.toString().replace(/\/([^\/]+)\/?$/, '')
@@ -13,7 +13,7 @@ export default function extendRoutine(routine, types = []){
       return Object.assign(result, {
         [toCamelCase(stage)]: actionCreator,
         [stage.toUpperCase()]: actionCreator.toString(),
-      });
+      })
     },
     routine
   );
