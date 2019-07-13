89243e9cce7ae3d8e2b98e3ed4f367b66009d331
Yury Shapkarin <yury@shapkarin.me>
2019-07-13 15:09:46 +0300

    refact

--- src/createExtendedRoutine.js
+++ src/createExtendedRoutine.js
@@ -1,17 +1,14 @@
 import { createRoutineCreator, defaultRoutineStages } from 'redux-saga-routines';
 
-import toArray from './toArray';
-
 export default function createExtendedRoutine(typePrefix, stages, payloadCreator, metaCreator){
-  const stagesArray = toArray(stages);
-  
   if(typeof typePrefix !== 'string'){
     throw new Error('`typePrefix` must be a string');
   }
-  if(!stages || stagesArray.length === 0){
+  if(!stages || stages.length === 0){
     throw new Error('`stages` must not be empty');
   }
 
-  const allStages = [...defaultRoutineStages, ...stagesArray];
+  const allStages = defaultRoutineStages.concat(stages);
+  
   return createRoutineCreator(allStages)(typePrefix, payloadCreator, metaCreator);
 };
--- src/extendRoutine.js
+++ src/extendRoutine.js
@@ -1,17 +1,15 @@
 import { createAction } from 'redux-actions';
 import { createRoutineCreator } from 'redux-saga-routines';
 
-import toArray from './toArray';
-
 export default function extendRoutine(routine, stages, payloadCreator, metaCreator){
   if(routine === undefined){
     throw new Error('`routine` must not be empty')
   }
-  if(stages === undefined){
+  if(stages === undefined || stages.length === 0){
     throw new Error('`stages` must not be empty')
   }
 
-  const allStages = [...routine._STAGES, ...toArray(stages)];
-  
+  const allStages = routine._STAGES.concat(stages);
+
   return createRoutineCreator(allStages)(routine._PREFIX, payloadCreator, metaCreator);
 }
