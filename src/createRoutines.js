ab51a59a9c53df72a74d6e23df894df92dbe0c21
Yury Shapkarin <yury@shapkarin.me>
2020-05-11 07:50:20 +0300

    createRoutines first version #15

--- src/createRoutines.js
+++ src/createRoutines.js
@@ -2,54 +2,57 @@ import { createRoutineCreator, defaultRoutineStages } from 'redux-saga-routines'
 
 export default function createRoutines(scheme){
   if(typeof scheme !== 'object' || Array.isArray(scheme)){
-    throw new Error('`typePrefix` must be an object');
+    throw new Error('`scheme` must be an object');
   };
 
   let result = {};
 
   function iterate(obj){
-    for(let [prefix, inside] of Object.entries(obj)) {
-      if(prefix.startsWith('_')) {
+    for(let [nameSpace, value] of Object.entries(obj)) {
+      if(nameSpace.startsWith('_')) {
         continue;
       }
-      
-      let payloadAndMeta = {payload: () => {}, meta: () => {}};
-      if(inside != null){
-        payloadAndMeta = Object.keys(inside).reduce((acc, cur) => 
-          cur.startsWith('_') ? 
-            { ...acc,
-              payload: { ...acc.payload, [cur.substring(1).toLowerCase()]: inside[cur].payload },
-              meta: { ...acc.meta, [cur.substring(1).toLowerCase()]: inside[cur].meta }
-            } 
-            :
-            acc,
-          payloadAndMeta
-        );
+      let allStages = [];
+      let payloadAndMeta = [];
+      let customStages = [];
+      let method = '';
+      let inside = null;
+
+      if(Array.isArray(value)){
+        method = value[0].method;
+        inside = value[1];
+      } else {
+        inside = value;
       }
 
-      result[prefix] = createRoutineCreator(defaultRoutineStages)(prefix, {...payloadAndMeta});
+      if(inside != null){
+        let insideStages = Object.keys(inside).filter(i => i.startsWith('_'))
+        customStages = insideStages.reduce((acc, cur) => !defaultRoutineStages.includes(cur) ? [...acc, cur.substring(1)] : acc ,[]);
+        payloadAndMeta = insideStages.reduce((acc, cur) => 
+          [
+            { ...acc[0], [cur.substring(1).toLowerCase()]: inside[cur] !== null ? inside[cur][0] : null },
+            { ...acc[1], [cur.substring(1).toLowerCase()]: inside[cur] !== null ? inside[cur][1] : null }
+          ],
+          payloadAndMeta
+        );
+      };
 
-      if (typeof inside == "object" && inside != null){
+      switch(method) {
+        case 'custom':
+          allStages = customStages;
+          break;
+        default:
+          allStages = [...defaultRoutineStages, ...customStages]
+      }
+
+      result[nameSpace] = createRoutineCreator(allStages).apply(null, [nameSpace, ...payloadAndMeta]);
+
+      if (typeof inside === "object" && inside != null){
         iterate(inside);
-      } 
+      }
     }
   }
 
-  iterate({
-    people: {
-      search: {
-        _TRIGGER: {
-          payload: ({ phone }) => parseInt(phone, 2),
-          meta: () => ({ trigger: true })
-        },
-        otherNameSpace: null
-       },
-      add: null,
-      remove: null
-    }
-  });
-
-  console.log(result.search.trigger({ phone: 101010 }));
-  ///console.log(result);
-
+  iterate(scheme);
+  return scheme;
 };
--- test/createRoutines.js
+++ test/createRoutines.js
@@ -1,3 +1,35 @@
 import { createRoutines } from '../src';
 
-createRoutines({});
+const SCHEME = {
+  people: {
+    search: {
+      _ANY: null,
+      _TRIGGER: [
+        ({ number }) => number * 2,
+        () => ({ trigger: true })
+      ],
+      _REQUEST: [
+        null,
+        () => ({ ignoreCache: true })
+      ],
+      _OTHER_STAGE: null,
+      otherNameSpace: null
+    },
+    customRoutine: [
+      { method: 'custom' },
+      {
+        _OPEN: [
+          ({ number }) => number * 2
+        ],
+        _CLOSE: null
+      }
+    ],
+    remove: null
+  }
+}
+
+createRoutines(SCHEME);
+
+const RESULT = {
+  
+}
